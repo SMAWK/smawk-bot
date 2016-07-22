de7475d3968eb74d462587b72d387fca8a30e0bf
bmatt468 <bnmtthews@gmail.com>
2016-07-22 10:56:17 -0400

    start of package conversion

--- smawk.go
+++ smawk.go
@@ -1,4 +1,4 @@
-package main
+package smawk
 
 import (
     "gopkg.in/telegram-bot-api.v4"
@@ -6,38 +6,54 @@ import (
     "net/http"
 )
 
-func main() {
-    // Create the bot using the provided access token
-    bot, err := tgbotapi.NewBotAPI("249930361:AAHz1Gksb-eT0SQG47lDb7WbJxujr7kGCkU")
+// BotAPI allows you to interact with the Telegram Bot API.
+type SmawkBot struct {
+    API *tgbotapi.BotAPI
+}
+
+// Connect takes a provided access token, and returns a pointer
+// to the Telegram Bot API. This function must be called in order
+// to have access to any of the commands
+func Connect(tkn string, debug bool) (*SmawkBot) {
+    // Call the Telegram API wrapper and authenticate our Bot
+    bot, err := tgbotapi.NewBotAPI(tkn)
+
+    // Check to see if there were any errors with our bot and fail
+    // if there were
     if err != nil {
         log.Fatal(err)
     }
 
-    // Tell the bot that we do not want to dump stuff to the log (console)
-    bot.Debug = false
+    if (debug) {
+        // Print confirmation
+        log.Printf("Authorized on account %s", bot.Self.UserName)
+    }
 
-    // Print confirmation
-    log.Printf("Authorized on account %s", bot.Self.UserName)
+    // Set our bot to either be in debug mode (everything gets put out to the console)
+    // or non debug mode (everything is silent)
+    bot.Debug = debug
 
-    _, err = bot.SetWebhook(tgbotapi.NewWebhookWithCert("https://mysimplethings.xyz:8443/309LKj2390gklj1LJF2", "smawk_cert.pem"))
+    // Create the SmawkBot
+    sbot := &SmawkBot {
+        API: bot,
+    }
+
+    // Return our bot back to the caller
+    return sbot
+}
+
+// OpenWebhookWithCert is the wrapper function that calls Telegram's Bot API
+// and listens to a self-signed https webhook for commands
+func (bot *SmawkBot) OpenWebhookWithCert(url string, cert string) {
+    _, err := bot.API.SetWebhook(tgbotapi.NewWebhookWithCert(url, cert))
     if err != nil {
         log.Fatal(err)
     }
+}
 
+func (bot *SmawkBot) Listen(token string) {
     // Start listening on our webhook for the commands
     // Spin off a goroutine to handle listening elsewhere
-    updates := bot.ListenForWebhook("/309LKj2390gklj1LJF2")
+    //updates := bot.API.ListenForWebhook("/309LKj2390gklj1LJF2")
     go http.ListenAndServeTLS("0.0.0.0:8443", "smawk_cert.pem", "smawk_key.pem", nil)
-
-    // Parse and execute each of the commands that come through the pipe
-    for update := range updates {
-        cmd := update.Message.Text
-        if (cmd == "/start" || cmd == "/start@smawk_bot") {
-            msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Lo, the official SMÄWKBot rises!")
-            bot.Send(msg)
-        } else if (cmd == "/hello" || cmd == "/hello@smawk_bot") {
-            msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Hello, @" + update.Message.From.UserName + "!")
-            bot.Send(msg)
-        }
-    }
 }
--- smawk_test.go
+++ smawk_test.go
@@ -1,4 +1,4 @@
-package main_test
+package smawk_test
 
 import (
 	"encoding/json"
